8176aab32766a3e44c155a170f964ff0dc00ee34
ferryirawan <27ferryirawan@gmail.com>
2019-07-12 11:02:01 +0700

    add next button

--- AttactOnProject/AttactOnProject/GameScene.swift
+++ AttactOnProject/AttactOnProject/GameScene.swift
@@ -14,11 +14,22 @@ class GameScene: SKScene {
     let goddess = SKSpriteNode(imageNamed: "human")
     
     override func didMove(to view: SKView) {
-        addChild(goddess)
-        goddess.xScale = 0.50
-        goddess.yScale = 0.50
-        goddess.position = CGPoint(x: 0, y: 0)
+//        addChild(goddess)
+//        goddess.xScale = 0.50
+//        goddess.yScale = 0.50
+//        goddess.position = CGPoint(x: 0, y: 0)
+//        backgroundColor = UIColor.orange
         
-        backgroundColor = UIColor.orange
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "yoona")
+        backgroundImage.contentMode =  UIView.ContentMode.scaleAspectFill
+        self.view?.insertSubview(backgroundImage, at: 0)
+        
+        let nextView = UIView(frame: CGRect(x: self.frame.size.width+88, y: self.frame.size.width/2-20, width: 44, height: 44))
+        let nextBtn = UIButton(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 44, height: 44)))
+        nextBtn.setImage(UIImage(named: "right-arrow"), for: .normal)
+//        nextBtn.addTarget(self, action: #selector(goToNotification), for: .touchUpInside)
+        nextView.addSubview(nextBtn)
+        view.addSubview(nextView)
     }
 }
--- AttactOnProject/AttactOnProject/GameViewController.swift
+++ AttactOnProject/AttactOnProject/GameViewController.swift
@@ -27,11 +27,9 @@ class GameViewController: UIViewController {
                 // Present the scene
                 if let view = self.view as! SKView? {
                     view.presentScene(sceneNode)
-                    
                     view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
+//                    view.showsFPS = true
+//                    view.showsNodeCount = true
                 }
             }
         }

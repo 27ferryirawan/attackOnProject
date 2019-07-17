2d1709a216dd59e9c32a76818e2dbd19f507f3ea
ferryirawan <27ferryirawan@gmail.com>
2019-07-17 14:26:12 +0700

    clean code Final Game

--- AttactOnProject/View/Finish Game/FinishGame.swift
+++ AttactOnProject/View/Finish Game/FinishGame.swift
@@ -19,69 +19,111 @@ class FinishGame: SKScene {
     let midView = SKSpriteNode(imageNamed: "box-stacked")
     let totalMoneyBox = SKSpriteNode(imageNamed: "moneymoneymakeyousmile")
     
-    
+    var energyText : SKLabelNode!
+    var totalMoneyText : SKLabelNode!
     var moneyCompletionText : SKLabelNode!
     
     override func didMove(to view: SKView) {
-        //backround sizing and positioning
-        background.zPosition = 0
-        background.position = CGPoint(x: frame.midX, y: frame.midY)
-        background.size = CGSize(width: frame.width, height: frame.height)
+        displayBckg()
+        displayMidView()
+        displayNextCloseBtn()
+        displayEnergy()
+        displayMoneyCompletion()
+        displayTotalMoney()
+    }
+    
+    func displayEnergy(){
+        //totalmoney Completion Text  styling
+        energyText = SKLabelNode(fontNamed: "Arial")
+        energyText.text = "5/5"
+        energyText.fontSize = 23
+        energyText.fontColor = .white
+        energyText.position = CGPoint(x: midView.frame.midX+10, y: midView.frame.midY+midView.frame.height/25)
+        energyText.zPosition = 2
         
+        addChild(energyText)
+    }
+    
+    func displayMidView(){
         //midMid styling
         midView.zPosition = 1
         midView.position = CGPoint(x: frame.midX-8 , y: frame.midY+24)
         midView.size = CGSize(width: frame.height*1.25, height: frame.height*1.15-50)
         
+        addChild(midView)
+    }
+    
+    func displayTotalMoney(){
+        
+        //total money box styling
+        totalMoneyBox.zPosition = 2
+        totalMoneyBox.position = CGPoint(x: background.frame.maxX - 125, y: background.frame.maxY - 25)
+        totalMoneyBox.zPosition = 2
+        
+        //totalmoney Completion Text  styling
+        totalMoneyText = SKLabelNode(fontNamed: "Arial")
+        totalMoneyText.text = "200.000.000"
+        totalMoneyText.fontSize = 17
+        totalMoneyText.fontColor = .white
+        totalMoneyText.position = CGPoint(x: background.frame.maxX - 112, y: background.frame.maxY - 30)
+        totalMoneyText.zPosition = 3
+        
+        addChild(totalMoneyText)
+        addChild(totalMoneyBox)
+    }
+    func displayBckg(){
+        //backround sizing and positioning
+        background.zPosition = 0
+        background.position = CGPoint(x: frame.midX, y: frame.midY)
+        background.size = CGSize(width: frame.width, height: frame.height)
+        
+        addChild(background)
+    }
+    func displayNextCloseBtn(){
         //close button styling
         closeBtn.zPosition = 2
         closeBtn.size = CGSize(width: midView.frame.width/2-40, height: midView.frame.height/5)
         closeBtn.position = CGPoint(x: midView.frame.midX-midView.frame.width/5+4, y: midView.frame.minY+closeBtn.frame.height/4)
         closeBtn.name = "closeButton"
         
-        
+
         //next button styling
         nextBtn.zPosition = 2
         nextBtn.size = CGSize(width: midView.frame.width/2-40, height: midView.frame.height/5)
         nextBtn.position = CGPoint(x: midView.frame.midX+midView.frame.width/5+4, y: midView.frame.minY+nextBtn.frame.height/4)
         nextBtn.name = "nextButton"
         
+        addChild(nextBtn)
+        addChild(closeBtn)
+    }
+    
+    func displayMoneyCompletion(){
         //money Completion Text  styling
         moneyCompletionText = SKLabelNode(fontNamed: "Arial")
         moneyCompletionText.text = "+50.000"
         moneyCompletionText.fontSize = 40
         moneyCompletionText.fontColor = .white
-        moneyCompletionText.position = CGPoint(x: frame.midX, y: frame.midY/2+12)
+        moneyCompletionText.position = CGPoint(x: frame.midX, y: midView.frame.midY-midView.frame.height/4-10)
         moneyCompletionText.zPosition = 2
         
-        //total money box styling
-        totalMoneyBox.zPosition = 1
-        totalMoneyBox.position = CGPoint(x: background.frame.maxX - 125, y: background.frame.maxY - 25)
-        totalMoneyBox.zPosition = 2
-        
-        addChild(totalMoneyBox)
         addChild(moneyCompletionText)
-        addChild(background)
-        addChild(midView)
-        addChild(nextBtn)
-        addChild(closeBtn)
     }
-    
-    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
             let touchedNode = atPoint(location)
-            if touchedNode.name == "closeButton" {
-                self.goBackToHomeScene()
+            if touchedNode.name == "nextButton" {
+                self.goToNextScene()
             }
         }
     }
     
-    func goBackToHomeScene() {
+    func goToNextScene() {
         let transition:SKTransition = SKTransition.fade(withDuration: 1)
-        let scene:SKScene = GameScene(size: self.size)
+        let scene:SKScene = TutorialSceneOne(size: self.size)
         self.view?.presentScene(scene, transition: transition)
     }
+    
+    
 }
--- AttactOnProject/View/Tutorial One/TutorialSceneOne.swift
+++ AttactOnProject/View/Tutorial One/TutorialSceneOne.swift
@@ -6,4 +6,45 @@
 //  Copyright © 2019 Ahmad Nizar. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
+import GameplayKit
+
+class TutorialSceneOne: SKScene {
+    
+    let background = SKSpriteNode(imageNamed: "Team-Detail-View-Blank")
+    let nextButton = SKSpriteNode(imageNamed: "right-arrow")
+    let tappedButton = SKView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+    
+    override func didMove(to view: SKView) {
+        //background styling
+        background.zPosition = 0
+        background.position = CGPoint(x: frame.midX, y: frame.midY)
+        background.size = CGSize(width: frame.width, height: frame.height)
+        
+        //next button styling
+        nextButton.position = CGPoint(x: frame.maxX*0.9, y: frame.maxY*0.1)
+        nextButton.name = "nextButton"
+        nextButton.size = CGSize(width: nextButton.size.width * 0.1, height: nextButton.size.height * 0.1)
+        
+        addChild(background)
+        addChild(nextButton)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            let touchedNode = atPoint(location)
+            if touchedNode.name == "nextButton" {
+                // Call the function here.
+                self.goToNextScene()
+            }
+        }
+    }
+    
+    func goToNextScene() {
+        let transition:SKTransition = SKTransition.fade(withDuration: 1)
+        let scene:SKScene = GamePlayScene(size: self.size)
+        self.view?.presentScene(scene, transition: transition)
+    }
+}
+

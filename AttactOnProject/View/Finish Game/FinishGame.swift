210d041ee3204d8241b2b2f4e35a6334de95e7ab
ferryirawan <27ferryirawan@gmail.com>
2019-07-17 10:35:59 +0700

    fix finish game scene

--- AttactOnProject/View/Finish Game/FinishGame.swift
+++ AttactOnProject/View/Finish Game/FinishGame.swift
@@ -17,6 +17,7 @@ class FinishGame: SKScene {
     let nextBtn = SKSpriteNode(imageNamed: "next-button")
     let closeBtn = SKSpriteNode(imageNamed: "close-button")
     let midView = SKSpriteNode(imageNamed: "box-stacked")
+    var moneyCompletionText : SKLabelNode!
     
     override func didMove(to view: SKView) {
         //backround sizing and positioning
@@ -26,8 +27,8 @@ class FinishGame: SKScene {
         
         //midMid styling
         midView.zPosition = 1
-        midView.position = CGPoint(x: frame.midX , y: frame.midY+18)
-        midView.size = CGSize(width: frame.height*1.05, height: frame.height*1.05-50)
+        midView.position = CGPoint(x: frame.midX-8 , y: frame.midY+24)
+        midView.size = CGSize(width: frame.height*1.25, height: frame.height*1.15-50)
         
         //close button styling
         closeBtn.zPosition = 2
@@ -42,6 +43,15 @@ class FinishGame: SKScene {
         nextBtn.position = CGPoint(x: midView.frame.midX+midView.frame.width/5+4, y: midView.frame.minY+nextBtn.frame.height/4)
         nextBtn.name = "nextButton"
         
+        //money Completion Text  styling
+        moneyCompletionText = SKLabelNode(fontNamed: "Arial")
+        moneyCompletionText.text = "+50.000"
+        moneyCompletionText.fontSize = 40
+        moneyCompletionText.fontColor = .white
+        moneyCompletionText.position = CGPoint(x: frame.midX, y: frame.midY/2+12)
+        moneyCompletionText.zPosition = 2
+        
+        addChild(moneyCompletionText)
         addChild(background)
         addChild(midView)
         addChild(nextBtn)

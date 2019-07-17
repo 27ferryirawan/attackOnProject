24c928f01b14d9812feb5e3b3676ab79ffe4d810
Ahmad Nizar <ahmadnizar.owl@gmail.com>
2019-07-17 13:05:20 +0700

    -re init gameplay scene

--- AttactOnProject/AttactOnProject/GameViewController.swift
+++ AttactOnProject/AttactOnProject/GameViewController.swift
@@ -16,10 +16,10 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
         // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
+        if let scene = GKScene(fileNamed: "PreGameScene") {
             
             // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
+            if let sceneNode = scene.rootNode as! PreGameScene? {
                 // Set the scale mode to scale to fit the window
 //                sceneNode.scaleMode = .aspectFill
                 
